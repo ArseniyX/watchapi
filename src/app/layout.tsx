0f5594e7c48d890af717ca09d156ee6c533d168a
ArseniyDev <jkl@usa.com>
2025-10-24 19:59:19 +0400

    skip e2e

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -131,7 +131,7 @@ export default function RootLayout({
         />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#111" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="WatchAPI" />
         <link rel="apple-touch-icon" href="/icon-192.png" />
